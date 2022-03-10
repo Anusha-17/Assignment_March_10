0afa73a25929d58eeba204683ba2b0c1011d2532
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-03-10 18:15:54 +0530

    3D Array

---
+++ 3D_Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main()
+{
+  int test[2][3][2],i,j,k;
+  printf("Enter 12 values: \n");
+
+  for (i = 0; i < 2; ++i)
+  {
+    for (j = 0; j < 3; ++j)
+    {
+      for (k = 0; k < 2; ++k)
+      {
+        scanf("%d", &test[i][j][k]);
+      }
+    }
+  }
+  printf("\nDisplaying values:\n");
+  for (i = 0; i < 2; ++i)
+  {
+    for (j = 0; j < 3; ++j)
+    {
+      for (k = 0; k < 2; ++k)
+      {
+        printf("test[%d][%d][%d] = %d\n", i, j, k, test[i][j][k]);
+      }
+    }
+  }
+}
