818fc8725da943eea9ea221161850fac565559dc
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-03-10 18:24:41 +0530

    3D Array

---
+++ 3D_Array1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int test[2][3][2]={1,2,3,4,5,6,7};
+  int i,j,k;
+  for (i = 0; i < 2; ++i)
+  {
+    for (j = 0; j < 3; ++j)
+    {
+      for (k = 0; k < 2; ++k)
+      {
+        printf("test[%d][%d][%d] = %d\n", i,j,k,test[i][j][k]);
+      }
+    }
+  }
+}
